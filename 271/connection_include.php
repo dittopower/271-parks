9bec33363a3f602f6bbc56f3a885a7ebb18d9e83
Damon <dittopower@gmail.com>
2021-01-31 01:42:44 +1000

    Add docker-compose.yml to orchestrate

--- 271/connection_include.php
+++ 271/connection_include.php
@@ -1,6 +1,6 @@
 <?php 
 	
-	$mysqli = new mysqli('172.17.0.3', '271USER', '5Z3MxFIE5Lph1VWiRg6ED', '271PARKS');
+	$mysqli = new mysqli('mysql', '271USER', '5Z3MxFIE5Lph1VWiRg6ED', '271PARKS');
 
 	if ($mysqli->connect_error) {
 		die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
